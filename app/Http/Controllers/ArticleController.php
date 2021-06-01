eaf7fca975d18402a6513215aadd7f9e8dc9435d
ardilalukita14 <lukitasari623@gmail.com>
2021-06-01 13:34:21 +0700

    Membuat controller untuk halaman artikel

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,86 +13,19 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function artikel1()
     {
-        $article = Article::find($id);
-        return view('sebelum.article.detail', ['article' => $article]);
+        return view('sebelum.article.artikel1');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function artikel2()
     {
-        //
+        return view('sebelum.article.artikel2');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function artikel3()
     {
-        //
+        return view('sebelum.article.artikel3');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $article = Article::find($id);
-        return view('sebelum.article.detail', compact('article'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-    // public function tampil(Request $request)
-    // {
-    //     if ($request->has('search')) { // Jika ingin melakukan pencarian judul
-    //         $datas = DataPetani::where('nama', 'like', "%" . $request->search . "%")->paginate(5);
-    //     } else { // Jika tidak melakukan pencarian judul
-    //         //fungsi eloquent menampilkan data menggunakan pagination
-    //         $datas = DataPetani::orderBy('id_petani', 'desc')->paginate(5); // Pagination menampilkan 5 data
-    //     }
-    //     return view('sebelum.dataPetani.tampil', compact('datas'));
-    // }
 }
+       
